cbfd478fc2fccbfcb1a93535f5ac9338e7e3c83d
YuqiLi <YuqiLi@node0.yuqili-196210.adslremotememory-pg0.utah.cloudlab.us>
2024-03-14 05:49:42 -0600

    dev/fake_swap_file

--- directswap/directswap.c
+++ directswap/directswap.c
@@ -7,6 +7,8 @@
 #include <linux/smp.h>
 #include <linux/swap.h>
 #include <linux/swapops.h>
+#include <linux/swapfile.h>
+#include <linux/frontswap.h>
 
 bool __direct_swap_enabled = false;
 EXPORT_SYMBOL(__direct_swap_enabled);
@@ -16,48 +18,114 @@ EXPORT_SYMBOL(kfifos_alloc);
 struct kfifo kfifos_free[NUM_KFIFOS_FREE];
 EXPORT_SYMBOL(kfifos_free);
 
-SYSCALL_DEFINE1(set_direct_swap_enabled, int, enable)
+static struct swap_info_struct *alloc_swap_info_with_type(int type);
+static void enable_swap_info(struct swap_info_struct *p, int prio,
+				unsigned char *swap_map,
+				struct swap_cluster_info *cluster_info,
+				unsigned long *frontswap_map);
+static void setup_swap_info(struct swap_info_struct *p, int prio,
+			    unsigned char *swap_map,
+			    struct swap_cluster_info *cluster_info);
+
+SYSCALL_DEFINE1(set_direct_swap_enabled, const char __user *, specialfile)
 {
-	int ret, i, type;
-	if(enable) {
-		for(i = 0; i < NUM_KFIFOS_ALLOC; i++) {
-			ret = kfifo_alloc(kfifos_alloc + i, sizeof(swp_entry)*PAGES_PER_KFIFO_ALLOC, GFP_KERNEL);
-			if(unlikely(ret)) {
-				printk("Alloc memory for kfifos_alloc failed with error code %d.", ret);
-				return ret;
-			}
+	int ret, i, type, prio, error;
+	struct swap_info_struct *p;
+	struct filename *name;
+	struct file *swap_file = NULL;
+	struct swap_cluster_info *cluster_info = NULL;
+	struct address_space *mapping;
+	unsigned long *frontswap_map = NULL;
+	unsigned char *swap_map = NULL;
+	int maxpages = NUM_PAGES_PER_REMOTE_SWAP_AREA;
+
+
+	for(i = 0; i < NUM_KFIFOS_ALLOC; i++) {
+		ret = kfifo_alloc(kfifos_alloc + i, sizeof(swp_entry)*PAGES_PER_KFIFO_ALLOC, GFP_KERNEL);
+		if(unlikely(ret)) {
+			printk("Alloc memory for kfifos_alloc failed with error code %d.", ret);
+			return ret;
 		}
-		for(i = 0; i < NUM_KFIFOS_FREE; i++) {
-			ret = kfifo_alloc(kfifos_free + i, sizeof(swp_entry)*PAGES_PER_KFIFO_FREE, GFP_KERNEL);
-			if(unlikely(ret)) {
-				printk("Alloc memory for kfifos_free failed with error code %d.", ret);
-				return ret;
-			}
+	}
+	for(i = 0; i < NUM_KFIFOS_FREE; i++) {
+		ret = kfifo_alloc(kfifos_free + i, sizeof(swp_entry)*PAGES_PER_KFIFO_FREE, GFP_KERNEL);
+		if(unlikely(ret)) {
+			printk("Alloc memory for kfifos_free failed with error code %d.", ret);
+			return ret;
 		}
-		for(i = 0; i < NUM_REMOTE_SWAP_AREA; i++) {
-			type = MAX_SWAPFILES - i - 1;
-			ret = init_swap_address_space(type, NUM_PAGES_PER_REMOTE_SWAP_AREA);
-			if(unlikely(ret)) {
-				printk("init remote swap address space failed with error code %d.", ret);
-				return ret;
-			}
-		}
-	} else {
-		for(i = 0; i < NUM_KFIFOS_ALLOC; i++) {
-			kfifo_free(kfifos_alloc + i);
-		}
-		for(i = 0; i < NUM_KFIFOS_FREE; i++) {
-			kfifo_free(kfifos_free + i);
-		}
-		for(i = 0; i < NUM_REMOTE_SWAP_AREA; i++) {
-			type = MAX_SWAPFILES - i - 1;
-			exit_swap_address_space(type);
+	}
+	for(i = 0; i < NUM_REMOTE_SWAP_AREA; i++) {
+		type = MAX_SWAPFILES - i - 1;
+		ret = init_swap_address_space(type, NUM_PAGES_PER_REMOTE_SWAP_AREA);
+		if(unlikely(ret)) {
+			printk("init remote swap address space failed with error code %d.", ret);
+			return ret;
 		}
 	}
 
- 	__direct_swap_enabled = enable;
-    printk("DirectSwap enable: %s\n", enable ? "enabled" : "disabled");
+	p = alloc_swap_info_with_type(MAX_SWAPFILES - NUM_REMOTE_SWAP_AREA);
+	if (IS_ERR(p))
+		return PTR_ERR(p);
+
+	name = getname(specialfile);
+	if (IS_ERR(name)) {
+		error = PTR_ERR(name);
+		name = NULL;
+		goto bad_set;
+	}
+	swap_file = file_open_name(name, O_RDWR|O_LARGEFILE, 0);
+	if (IS_ERR(swap_file)) {
+		error = PTR_ERR(swap_file);
+		swap_file = NULL;
+		goto bad_set;
+	}
+	p->swap_file = swap_file;
+	prio = -1;
+	swap_map = vzalloc(maxpages);
+	if (!swap_map) {
+		error = -ENOMEM;
+		goto bad_set;
+	}
+	if (IS_ENABLED(CONFIG_FRONTSWAP))
+	frontswap_map = kvcalloc(BITS_TO_LONGS(maxpages),
+					sizeof(long),
+					GFP_KERNEL);
+	enable_swap_info(p, prio, swap_map, cluster_info, frontswap_map);
+
+ 	__direct_swap_enabled = 1;
+    printk("DirectSwap enabled successfully.");
  	return 0;
+
+bad_set:
+	return -1;
+}
+
+SYSCALL_DEFINE1(set_direct_swap_disabled, const char __user *, specialfile)
+{
+	struct swap_info_struct *p = swap_info[MAX_SWAPFILES - NUM_REMOTE_SWAP_AREA];
+	int i, type;
+	if(!p) {
+		printk("No DirectSwap area found.");
+		return -1;
+	}
+	for(i = 0; i < NUM_KFIFOS_ALLOC; i++) {
+		kfifo_free(kfifos_alloc + i);
+	}
+	for(i = 0; i < NUM_KFIFOS_FREE; i++) {
+		kfifo_free(kfifos_free + i);
+	}
+	for(i = 0; i < NUM_REMOTE_SWAP_AREA; i++) {
+		type = MAX_SWAPFILES - i - 1;
+		exit_swap_address_space(type);
+	}
+	
+	vfree(p->swap_map);
+	kvfree(p->cluster_info);
+	kvfree(p->frontswap_map);
+
+	__direct_swap_enabled = 0;
+	
+	return 0;
 }
 
 int direct_swap_alloc_remote_pages(int n_goal, unsigned long entry_size, swp_entry_t swp_entries[]) {
@@ -85,6 +153,79 @@ int direct_swap_free_remote_page(swp_entry_t entry) {
 		return 0;
 	}
 }
+
+static struct swap_info_struct *alloc_swap_info_with_type(int type) {
+	struct swap_info_struct *p;
+	struct swap_info_struct *defer = NULL;
+	int i;
+
+	p = kvzalloc(struct_size(p, avail_lists, nr_node_ids), GFP_KERNEL);
+	if (!p)
+		return ERR_PTR(-ENOMEM);
+
+	spin_lock(&swap_lock);
+	if (type >= MAX_SWAPFILES) {
+		spin_unlock(&swap_lock);
+		percpu_ref_exit(&p->users);
+		kvfree(p);
+		return ERR_PTR(-EPERM);
+	}
+	p->type = type;
+	p->swap_extent_root = RB_ROOT;
+	plist_node_init(&p->list, 0);
+	for_each_node(i)
+		plist_node_init(&p->avail_lists[i], 0);
+	p->flags = SWP_USED;
+	spin_unlock(&swap_lock);
+	
+	spin_lock_init(&p->lock);
+	spin_lock_init(&p->cont_lock);
+	init_completion(&p->comp);
+
+	return p;
+}
+
+static void enable_swap_info(struct swap_info_struct *p, int prio,
+				unsigned char *swap_map,
+				struct swap_cluster_info *cluster_info,
+				unsigned long *frontswap_map)
+{
+	frontswap_init(p->type, frontswap_map);
+	spin_lock(&swap_lock);
+	spin_lock(&p->lock);
+	setup_swap_info(p, prio, swap_map, cluster_info);
+	spin_unlock(&p->lock);
+	spin_unlock(&swap_lock);
+	/*
+	 * Finished initializing swap device, now it's safe to reference it.
+	 */
+	//percpu_ref_resurrect(&p->users);
+	spin_lock(&swap_lock);
+	spin_lock(&p->lock);
+	//_enable_swap_info(p);
+	spin_unlock(&p->lock);
+	spin_unlock(&swap_lock);
+}
+
+static void setup_swap_info(struct swap_info_struct *p, int prio,
+			    unsigned char *swap_map,
+			    struct swap_cluster_info *cluster_info)
+{
+	int i;
+
+	if (prio >= 0)
+		p->prio = prio;
+	else
+		p->prio = -999;
+	/*
+	 * the plist prio is negated because plist ordering is
+	 * low-to-high, while swap ordering is high-to-low
+	 */
+	p->list.prio = -p->prio;
+	p->swap_map = swap_map;
+	p->cluster_info = cluster_info;
+}
+
 
 inline bool is_direct_swap_area(int type)
 {
--- include/linux/syscalls_directswap.h
+++ include/linux/syscalls_directswap.h
@@ -1,6 +1,7 @@
 #ifndef _LINUX_SYSCALLS_DIRECTSWAP_H
 #define _LINUX_SYSCALLS_DIRECTSWAP_H
 
-asmlinkage long sys_set_direct_swap_enabled(int enable);
+asmlinkage long sys_set_direct_swap_enabled(const char __user *specialfile);
+asmlinkage long sys_set_direct_swap_disabled(const char __user *specialfile);
 
 #endif
