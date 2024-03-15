eaf2503963d5f780e7425d0a0c5c588d90b61b38
YuqiLi <YuqiLi@node0.yuqili-196333.adslremotememory-pg0.utah.cloudlab.us>
2024-03-15 01:48:33 -0600

    dev/add setup_swap_map_and_extents

--- directswap/directswap.c
+++ directswap/directswap.c
@@ -28,6 +28,11 @@ static void enable_swap_info(struct swap_info_struct *p, int prio,
 static void setup_swap_info(struct swap_info_struct *p, int prio,
 			    unsigned char *swap_map,
 			    struct swap_cluster_info *cluster_info);
+static int setup_swap_map_and_extents(struct swap_info_struct *p,
+					unsigned char *swap_map,
+					struct swap_cluster_info *cluster_info,
+					unsigned long maxpages,
+					sector_t *span)
 
 SYSCALL_DEFINE1(set_direct_swap_enabled, const char __user *, specialfile)
 {
@@ -40,6 +45,7 @@ SYSCALL_DEFINE1(set_direct_swap_enabled, const char __user *, specialfile)
 	unsigned long *frontswap_map = NULL;
 	unsigned char *swap_map = NULL;
 	int maxpages = NUM_PAGES_PER_REMOTE_SWAP_AREA;
+	sector_t span;
 
 
 	for(i = 0; i < NUM_KFIFOS_ALLOC; i++) {
@@ -93,6 +99,13 @@ SYSCALL_DEFINE1(set_direct_swap_enabled, const char __user *, specialfile)
 	if (!swap_map) {
 		error = -ENOMEM;
 		printk(KERN_ERR "Alloc space for swap_map failed.");
+		goto bad_set;
+	}
+	nr_extents = setup_swap_map_and_extents(p, swap_map,
+		cluster_info, maxpages, &span);
+	if (unlikely(nr_extents < 0)) {
+		error = nr_extents;
+		printk(KERN_ERR "Setup swap_map and extents failed.");
 		goto bad_set;
 	}
 
@@ -217,6 +230,71 @@ static void enable_swap_info(struct swap_info_struct *p, int prio,
 	//_enable_swap_info(p);
 	spin_unlock(&p->lock);
 	spin_unlock(&swap_lock);
+}
+
+static int setup_swap_map_and_extents(struct swap_info_struct *p,
+					unsigned char *swap_map,
+					struct swap_cluster_info *cluster_info,
+					unsigned long maxpages,
+					sector_t *span)
+{
+	unsigned int j, k;
+	unsigned int nr_good_pages;
+	int nr_extents;
+	unsigned long nr_clusters = DIV_ROUND_UP(maxpages, SWAPFILE_CLUSTER);
+	unsigned long col = p->cluster_next / SWAPFILE_CLUSTER % SWAP_CLUSTER_COLS;
+	unsigned long i, idx;
+
+	nr_good_pages = maxpages - 1;	/* omit header page */
+
+	cluster_list_init(&p->free_clusters);
+	cluster_list_init(&p->discard_clusters);
+
+	/* Haven't marked the cluster free yet, no list operation involved */
+	for (i = maxpages; i < round_up(maxpages, SWAPFILE_CLUSTER); i++)
+		inc_cluster_info_page(p, cluster_info, i);
+
+	if (nr_good_pages) {
+		swap_map[0] = SWAP_MAP_BAD;
+		/*
+		 * Not mark the cluster free yet, no list
+		 * operation involved
+		 */
+		inc_cluster_info_page(p, cluster_info, 0);
+		p->max = maxpages;
+		p->pages = nr_good_pages;
+		nr_extents = setup_swap_extents(p, span);
+		if (nr_extents < 0)
+			return nr_extents;
+		nr_good_pages = p->pages;
+	}
+	if (!nr_good_pages) {
+		pr_warn("Empty swap-file\n");
+		return -EINVAL;
+	}
+
+	if (!cluster_info)
+		return nr_extents;
+
+
+	/*
+	 * Reduce false cache line sharing between cluster_info and
+	 * sharing same address space.
+	 */
+	for (k = 0; k < SWAP_CLUSTER_COLS; k++) {
+		j = (k + col) % SWAP_CLUSTER_COLS;
+		for (i = 0; i < DIV_ROUND_UP(nr_clusters, SWAP_CLUSTER_COLS); i++) {
+			idx = i * SWAP_CLUSTER_COLS + j;
+			if (idx >= nr_clusters)
+				continue;
+			if (cluster_count(&cluster_info[idx]))
+				continue;
+			cluster_set_flag(&cluster_info[idx], CLUSTER_FLAG_FREE);
+			cluster_list_add_tail(&p->free_clusters, cluster_info,
+					      idx);
+		}
+	}
+	return nr_extents;
 }
 
 static void setup_swap_info(struct swap_info_struct *p, int prio,
